13469be9d6b3e044e51dabd0a508d8732d185184
Martin6363 <martin.khachatryan.2024@gmail.com>
2024-05-21 16:14:35 +0400

    Added user auth

--- backend/database/migrations/2024_05_08_153522_create_images_table.php
+++ backend/database/migrations/2024_05_08_153522_create_images_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('image');
             $table->unsignedBigInteger('product_id');
-
+            
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\AuthUserController;
 use App\Http\Controllers\api\v1\ProductCategoryController;
 use App\Http\Controllers\api\v1\ProductController;
 use Illuminate\Http\Request;
@@ -9,7 +10,12 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::prefix('v1')->as('v1')->group(function () {
+    Route::controller(AuthUserController::class)->group(function() {
+        Route::post('/register', 'register');
+        Route::post('/login', 'login')->name('login');
+    });
     ##Product Routes
     Route::resource('/products', ProductController::class);
     ##Category Routes
